96d39ec43973ff28f6c8ecd3a2d0806db6068088
Gabriel Turow <gabeturow@gmail.com>
2016-09-29 02:14:07 -0400

    Moving mouths to dialogue

---
+++ PortraitOfATyrantUpdated/Assets/AssetPackages/Standard Assets (Mobile)/Scripts/MoveMouthWithAudio.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveMouthWithAudio : MonoBehaviour {
+	SpectrumAnalysis spectrumAnalysis;
+	public Transform mouthToMove;
+	public string currentName;
+	AudioSource audioSourceDialogue;
+	// Use this for initialization
+	void Start () {
+		audioSourceDialogue=DialogueViewer.main.audioSource;
+		spectrumAnalysis=DialogueViewer.main.GetComponent<SpectrumAnalysis>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(DialogueViewer.main.characterName.text==currentName){
+			mouthToMove.localRotation = Quaternion.Euler(0.0f,0.0f,-spectrumAnalysis.rmsValue*1000);
+		}
+	}
+}
